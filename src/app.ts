afad1df27e96519e682201fec03774b6b59963d7
Ray Yamamoto Hilton <ray@wirestorm.net>
2021-06-30 04:29:25 +0000

    Add template project

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import compression from "compression";  // compresses requests
+import bodyParser from "body-parser";
+import lusca from "lusca";
+import flash from "express-flash";
+import path from "path";
+import passport from "passport";
+
+// Controllers (route handlers)
+import * as homeController from "./controllers/home";
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(lusca.xframe("SAMEORIGIN"));
+app.use(lusca.xssProtection(true));
+app.use((req, res, next) => {
+    res.locals.user = req.user;
+    next();
+});
+
+app.use(
+    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+);
+
+/**
+ * Primary app routes.
+ */
+app.get("/", homeController.index);
+
+export default app;
